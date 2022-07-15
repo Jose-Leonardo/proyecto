2cb7256e18ca03c97b097d97aebfa502e1d57112
José Leonardo <augl020225@gmail.com>
2022-07-14 19:50:15 -0700

    terminado- se puede mejorar

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const clothing = [
         name: "Sudadera",
         price: 1000,
         stock: 10,
+        pricetotal: 1000,
         img: "./img/featured1.png",
     },
     {
@@ -11,6 +12,7 @@ const clothing = [
         name: "camisa",
         price: 800,
         stock: 10,
+        pricetotal: 800,
         img: "./img/featured2.png",
     },
     {
@@ -18,18 +20,67 @@ const clothing = [
         name: "Sudadera",
         price: 1000,
         stock: 10,
+        pricetotal: 1000,
         img: "./img/featured3.png",
     },
 ];
 
 document.addEventListener("DOMContentLoaded", function(){
-
     CarritoDEropa();
 })
 
 function CarritoDEropa(){
     const armario = document.querySelector(".inventario");
-    let html = "";
+    const itearacarrito = document.querySelector(".menu-carrito");
+
+    let agregando = {};
+
+    itearacarrito.addEventListener("click",(evento) =>{
+                        if (evento.target.classList.contains("mas")) {
+                            const idobjeto = parseInt(evento.target.parentElement.id)
+                            agregando[idobjeto].cantidad++
+                            agregando[idobjeto].pricetotal += agregando[idobjeto].pricetotal
+                            console.log("es mas")
+                        }
+                        if (evento.target.classList.contains("menos")) {
+                            const idobjeto = parseInt(evento.target.parentElement.id)
+                            agregando[idobjeto].cantidad--
+                            agregando[idobjeto].pricetotal = agregando[idobjeto].pricetotal - agregando[idobjeto].price
+                            console.log("es menos")
+                        }
+                        if (evento.target.classList.contains("eliminar")) {
+                            const idobjeto = parseInt(evento.target.parentElement.id)
+                            delete agregando[idobjeto]
+                            console.log("es eliminar")
+                        }
+        funcion2()                
+    })
+    armario.addEventListener("click", function (event){
+        console.log("le diste a armario");
+        let aux = event.target.classList.contains("btn");
+        if (aux) {
+            const id = event.target.getAttribute('id')
+            const [filtrado] = clothing.filter((n) => n.id == id)
+            if (agregando[id]) {
+                if (agregando[id].cantidad <agregando[id].stock) {
+                    agregando[id].cantidad++
+                }else{
+                    alert("se termino el producto")
+                }
+        }else{
+            agregando[id] = filtrado
+            agregando[id].cantidad = 1
+        }
+        funcion2()
+        console.log(agregando)
+ 
+        itearacarrito.innerHTML = html2;
+        }
+        
+    })
+    
+    function funcion1(){
+            let html = "";
     clothing.forEach(element => {
         html += `<div class="ropa">
         <div class="ropa-imagen">
@@ -40,55 +91,39 @@ function CarritoDEropa(){
             <span class="stock" >|Stock: ${element.stock}</span>
         </h3>
         <h3 class="name">${element.name}</h3>
-        <button class="btn" data-id="${element.id}">Agregar</button>
+        <button class="btn" id="${element.id}">Agregar</button>
         </div>`
     });
     
     armario.innerHTML = html;
-    let agregando = [];
-    document.addEventListener("click", function (event){
-        let aux = event.target.classList.contains("btn");
-        let iduser = event.target.getAttribute('data-id')
-        let money =  event.target.parentElement.children[1].firstElementChild
-        let nombre = event.target.parentElement.children[2]
-        let foto = event.target.parentElement.children[0].children[0]
-        if (aux) {
-            const productoCarrito = {
-                img: foto.getAttribute("src"),
-                name: nombre.textContent,
-                id: iduser,
-                cantidad: 1,
-                price: money.textContent,
-                pricetotal: 1000
-            }
-            if (agregando.hasOwnProperty(productoCarrito.id)) {
-                productoCarrito.cantidad = agregando[productoCarrito.id].cantidad + 1
-                productoCarrito.pricetotal = agregando[productoCarrito.id].pricetotal + productoCarrito.pricetotal
-            }
-            agregando[productoCarrito.id] = {...productoCarrito}
-            console.log(iduser)
-            console.log(agregando)
-        }
-        const itearacarrito = document.querySelector(".menu-carrito");
-        let html2 = "";
-        agregando.forEach(element => {
-            html2 += `<div class="ropa">
-            <div class="ropa-imagen">
+    }       
+
+    function funcion2(){
+        const agregandoarray = Object.values(agregando)
+        let html2 = ``
+        agregandoarray.forEach((element) =>{ 
+            html2 += `
+            <div class="ropa2">
+            <div class="ropa-imagen2">
                 <img src=${element.img} alt="${element.name}">
-            </div>
+             </div>
             <h3 class="texto">
-                <font class="precio">${element.price}</font>
-                <span class="stock" >|Stock:10 </span>
+                <b>${element.name}</b>
+                <span class="stock" >Stock:10|<font class="precio">${element.price}</font> </span>
                 <font class="precio">Subtotal: $${element.pricetotal}</font>
                 <span class="stock" >${element.cantidad} unidades </span>
+                <div class="actions " id="${element.id}">
+                <span class="menos">-</span><b>${element.cantidad}</b><span class="mas">+</span><span class="eliminar">        X</span>
+                </div>
             </h3>
-            <h3 class="name">${element.name}</h3>
-            <button class="btn" data-id="${element.id}">Agregar</button>
-            </div>`
-        });
-        
-        itearacarrito.innerHTML = html2;
-    })
-   
+                
+            </div>
+            `
+        })
+            itearacarrito.innerHTML = html2;
+    }    
+
+    funcion1()
+
     }
 
